e1b555f1e175621f37239898b5583a4bb5de64ce
bugi-bit <85178323+bugi-bit@users.noreply.github.com>
2021-06-06 21:38:19 +0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -39,6 +39,25 @@ bot.onText(/\/predict/, (msg) => {
 bot.on('message', (msg) => {
     if(state == 1){
         console.log(msg.text);
+        s = msg.text.split("|")
+        i = s[0]
+        v = s[1]
+        model.predict(
+        [
+            parseFloat(s[0]), // string to float
+            parseFloat(s[1])
+        ]
+    ).then((jres)=>{
+        console.log(jres);
+        bot.sendMessage(
+            msg.chat.id,
+            `nilai V yang diprediksi adalah ${jres[0]} volt`
+    ); 
+        bot.sendMessage(
+            msg.chat.id,
+            `nilai P yang diprediksi adalah ${jres[1]} volt`
+    ); 
+    })
     }
     else{
         state = 0
