9487205384256388890e85a6b8e74396b3ff1e4f
bugi-bit <85178323+bugi-bit@users.noreply.github.com>
2021-06-20 02:23:08 +0700

    Update cls_model.js

--- api/sdk/cls_model.js
+++ api/sdk/cls_model.js
@@ -11,31 +11,31 @@ function normalized(data){ // i & r
 const argFact = (compareFn) => (array) => array.map((el, idx) => [el, idx]).reduce(compareFn)[1]
 const argMax = argFact((min, el) => (el[0] > min[0] ? el : min))
 
-// function ArgMax(res){
-//     label = "NORMAL"
-//     if(argMax(res) == 1) {
-//         label = "OVER VOLTAGE"
-//     }if(argMax(res) == 2) {
-//         label = "DROP VOLTAGE"
-//     }
-//     return label
 
 function ArgMax(res){
     label = "NORMAL"
-    cls_data = []
-    for(i=0; i<res.lenght; i++){
-        cls_data[i] = res[i]
+    if(argMax(res) == 1) {
+        label = "OVER VOLTAGE"
+    }if(argMax(res) == 2) {
+        label = "DROP VOLTAGE"
     }
+    return label
+
+// function ArgMax(res){
+//     label = "NORMAL"
+//     cls_data = []
+//     for(i=0; i<res.lenght; i++){
+//         cls_data[i] = res[i]
+//     }
 //     console.log(cls_data, argMax(cls_data));
     
-    if(argMax(cls_data) == 1){
-      label = "OVER VOLTAGE"
-    }if(argMax(cls_data) == 0){
-      label = "DROP VOLTAGE"
-    }
-    console.log(cls_data, argMax(cls_data));
-  return label
-}
+//     if(argMax(cls_data) == 1){
+//       label = "OVER VOLTAGE"
+//     }if(argMax(cls_data) == 0){
+//       label = "DROP VOLTAGE"
+//     }
+//      return label
+// }
 
 async function classify(data){
     let in_dim = 4;
